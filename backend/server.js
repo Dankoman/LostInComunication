5ab68c2ce3d612702ccc2fd4197903a7ac40c63a
Marqs <markus.rohsto@gmail.com>
2023-08-11 21:03:52 +0200

    tagit bort mongo koppling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const socketIO = require('socket.io');
 const cors = require('cors');
 
@@ -7,30 +6,28 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:3001' }));
 const port = 3000;
 
-mongoose.connect('mongodb://mongo:27017/game', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const MessageSchema = new mongoose.Schema({ content: String, timestamp: Date });
-const Message = mongoose.model('Message', MessageSchema);
-
 app.get('/', (req, res) => res.send('Backend is up and running!'));
 
 const server = app.listen(port, () => console.log(`Server running on port ${port}`));
 
-const io = socketIO(server);
-
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
 io.on('connection', (socket) => {
     console.log('User connected');
 
     socket.on('send-message', (data) => {
         const content = data.content;
 
-        // Logga när servern mottar ett meddelande
+        // Log when the server receives a message
         console.log('Received message from client:', content);
 
-        const message = new Message({ content: content, timestamp: new Date() });
-        message.save();
-
-        // Logga när servern skickar ett meddelande till alla klienter
+        // Log when the server sends a message to all clients
         console.log('Sending message to all clients:', content);
         io.emit('receive-message', { content: content });
     });
